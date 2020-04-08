273a4c3af6fcbda698dc37b1c0fd1be3817891dd
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-08 12:19:49 -0400

    [AN] Added Age calculation and setted that value to age text view.

--- app/src/main/java/com/example/taxcalculationspecification/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculationspecification/CRACustomer.java
@@ -2,13 +2,77 @@ package com.example.taxcalculationspecification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CRACustomer extends AppCompatActivity {
+
+    private TextView DateTxt;
+    private TextView Age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DateTxt = findViewById(R.id.BirthDate);
+        Age = findViewById(R.id.Age);
+
+
+        final Calendar myCalendar = Calendar.getInstance();
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                DateTxt.setText("Date of Birth: "+sdf.format(myCalendar.getTime()));
+                Age.setText("Age: "+Integer.toString(calculateAge(myCalendar.getTimeInMillis())));
+
+            }
+
+        };
+
+        DateTxt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(CRACustomer.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+
+        });
+
     }
+
+    int calculateAge(long date){
+        Calendar dob = Calendar.getInstance();
+        dob.setTimeInMillis(date);
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if(today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)){
+            age--;
+        }
+        return age;
+    }
+
+
+
 }
