c208bd675620b156f84d69b13840f51955ca24a1
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-08 01:40:17 -0400

    [AN] Created Splash Screen and added Firstname editable text

---
+++ app/src/main/java/com/example/taxcalculationspecification/splashScreenActivity.java
@@ -0,0 +1,40 @@
+package com.example.taxcalculationspecification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+
+        private static int SPLASH_SCREEN_TIME_OUT=3000;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+            setContentView(R.layout.activity_splash_screen);
+
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i=new Intent(splashScreenActivity.this,
+                            CRACustomer.class);
+
+                    startActivity(i);
+
+
+                    finish();
+
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
+        }
+
+}
