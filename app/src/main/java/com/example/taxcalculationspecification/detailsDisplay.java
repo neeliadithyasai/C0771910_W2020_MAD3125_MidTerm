16ac91c5c14430714dda8bf2b80993031b78132b
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-04-09 01:43:58 -0400

    [AN] Added function for calculating cpp

--- app/src/main/java/com/example/taxcalculationspecification/detailsDisplay.java
+++ app/src/main/java/com/example/taxcalculationspecification/detailsDisplay.java
@@ -8,7 +8,8 @@ import android.widget.TextView;
 
 public class detailsDisplay extends AppCompatActivity {
 
-    private TextView sno;
+    private TextView cpp;
+    private TextView eI;
 
 
     @Override
@@ -16,7 +17,8 @@ public class detailsDisplay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_display);
 
-      //  sno = findViewById(R.id.sinNumber);
+       cpp = findViewById(R.id.cppp);
+       eI = findViewById(R.id.eIp);
 
 
         Intent intent = getIntent();
@@ -24,9 +26,40 @@ public class detailsDisplay extends AppCompatActivity {
 
         CRACustomer C2 = (CRACustomer) extras.getParcelable("object");
 
+        Double gross = C2.getGrossIncome();
+
+       cpp.setText(calcualtecpp(gross).toString());
+       eI.setText(calculateEi(gross).toString());
+
+
+
       //  sno.setText(C2.getCurrentDate()+"\n"+C2.getSinNo()+"\n"+C2.getFirstName()+"\n"+C2.getLastName()+"\n"+C2.getBirthdate()+"\n"+C2.getAge()+"\n"+C2.getGender()+"\n"+C2.getGrossIncome()+"\n"+C2.getRRSP());
 
 
 
+
     }
+
+
+    private Double calcualtecpp(Double gross)
+    {
+        Double x;
+        Double y;
+        if(gross < 57400){
+            x = 57400 - gross;
+            y = 57400 -x;
+            x = y * 0.051;
+
+        }else
+        {
+            x = gross - 57400;
+            y = gross - x;
+            x = y * 0.051;
+        }
+
+        return x;
+
+    }
+   
+
 }
